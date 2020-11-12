8267f2b54e513e4631fc45f759dd4e2eb14a1fd5
IT03 <phuc@gmail.com>
2020-11-12 16:11:47 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <sunPage />
+  </div>
+</template>
+
+<script>
+import sunPage from './components/sun-page.vue'
+export default {
+  name: 'App',
+  data(){
+    return {
+      show: false
+    }
+  },
+  components:{
+    sunPage
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/sun-page.vue
@@ -0,0 +1,390 @@
+<template>
+  <div class="sun-page">
+    <div class="container">
+      <section class= 'sun'>
+        <div>
+          <div class="sun-header">
+            <img class="tokenLogo" src="../assets/tokenLogo1.png" alt="tokenLogo">
+            <img class="sunLogo" src="../assets/sunLogo.png" alt="sunLogo">
+            <div class="header-right">
+              <h1 class="title">SUN/TRX LP Pool</h1>
+              <div class="hr-sub">
+                Stake SUN/TRX LP Earn <b>9x</b> SUN
+              </div>
+            </div>
+          </div>
+          <div class="total">
+            <span class="t-num">Total: 54,462,041,155</span>
+            <span class="apy">APY: 37.70%</span>
+          </div>
+          <div class="info">
+            <div>
+              <p>Total staked</p>
+              <p>
+                <span>1,742,8517</span>
+                <span>SUN</span>
+              </p>
+              <p>
+                <span>561,446,660</span>
+                <span>TRX</span>
+              </p>
+            </div>
+            <div>
+              <p>Pool supply</p>
+              <p>
+                <span>23,127</span>
+                <span>SUN</span>
+              </p>
+            </div>
+          </div>
+          <div class="bottomFix">
+            <a href="#" class="type-link">Get LP Token</a>
+            <button class="create_btn">Select</button>
+          </div>
+        </div>
+      </section>
+      <section class= 'sun'>
+        <div>
+          <div class="sun-header">
+            <img class="tokenLogo" src="../assets/tokenLogo2.png" alt="tokenLogo">
+            <img class="sunLogo" src="../assets/sunLogo.png" alt="sunLogo">
+            <div class="header-right">
+              <h1 class="title">USDT/TRX LP Pool</h1>
+              <div class="hr-sub">
+                Stake USDT/TRX LP Earn <b>9x</b> SUN
+              </div>
+            </div>
+          </div>
+          <div class="total">
+            <span class="t-num">Total:875,645,180</span>
+            <span class="apy">APY: 19.30%</span>
+          </div>
+          <div class="info">
+            <div>
+              <p>Total staked</p>
+              <p>
+                <span>1,742,8517</span>
+                <span>SUN</span>
+              </p>
+              <p>
+                <span>561,446,660</span>
+                <span>TRX</span>
+              </p>
+            </div>
+            <div>
+              <p>Pool supply</p>
+              <p>
+                <span>23,127</span>
+                <span>SUN</span>
+              </p>
+            </div>
+          </div>
+          <div class="bottomFix">
+            <a href="#" class="type-link">Get LP Token</a>
+            <button class="create_btn">Select</button>
+          </div>
+        </div>
+      </section>
+      <section class= 'sun'>
+        <div>
+          <div class="sun-header">
+            <img class="tokenLogo" src="../assets/tokenLogo3.png" alt="tokenLogo">
+            <img class="sunLogo" src="../assets/sunLogo.png" alt="sunLogo">
+            <div class="header-right">
+              <h1 class="title">SUN/TRX LP Pool</h1>
+              <div class="hr-sub">
+                Stake SUN/TRX LP Earn <b>9x</b> SUN
+              </div>
+            </div>
+          </div>
+          <div class="total">
+            <span class="t-num">Total: 54,462,041,155</span>
+            <span class="apy">APY: 37.70%</span>
+          </div>
+          <div class="info">
+            <div>
+              <p>Total staked</p>
+              <p>
+                <span>1,742,8517</span>
+                <span>SUN</span>
+              </p>
+              <p>
+                <span>561,446,660</span>
+                <span>TRX</span>
+              </p>
+            </div>
+            <div>
+              <p>Pool supply</p>
+              <p>
+                <span>23,127</span>
+                <span>SUN</span>
+              </p>
+            </div>
+          </div>
+          <div class="bottomFix">
+            <a href="#" class="type-link">Get LP Token</a>
+            <button class="create_btn">Select</button>
+          </div>
+        </div>
+      </section>
+      <section class= 'sun'>
+        <div>
+          <div class="sun-header">
+            <img class="tokenLogo" src="../assets/tokenLogo4.png" alt="tokenLogo">
+            <img class="sunLogo" src="../assets/sunLogo.png" alt="sunLogo">
+            <div class="header-right">
+              <h1 class="title">USDJ/TRX LP Pool</h1>
+              <div class="hr-sub">
+                Stake WBTT/TRX LP Earn <b>1x</b> SUN
+              </div>
+            </div>
+          </div>
+          <div class="total">
+            <span class="t-num">Total: 54,462,041,155</span>
+            <span class="apy">APY: 37.70%</span>
+          </div>
+          <div class="info">
+            <div>
+              <p>Total staked</p>
+              <p>
+                <span>1,742,8517</span>
+                <span>SUN</span>
+              </p>
+              <p>
+                <span>561,446,660</span>
+                <span>TRX</span>
+              </p>
+            </div>
+            <div>
+              <p>Pool supply</p>
+              <p>
+                <span>23,127</span>
+                <span>SUN</span>
+              </p>
+            </div>
+          </div>
+          <div class="bottomFix">
+            <a href="#" class="type-link">Get LP Token</a>
+            <button class="create_btn">Select</button>
+          </div>
+        </div>
+      </section>
+      <section class= 'sun'>
+        <div>
+          <div class="sun-header">
+            <img class="tokenLogo" src="../assets/tokenLogo5.png" alt="tokenLogo">
+            <img class="sunLogo" src="../assets/sunLogo.png" alt="sunLogo">
+            <div class="header-right">
+              <h1 class="title">SUN/TRX LP Pool</h1>
+              <div class="hr-sub">
+                Stake SUN/TRX LP Earn <b>9x</b> SUN
+              </div>
+            </div>
+          </div>
+          <div class="total">
+            <span class="t-num">Total: 54,462,041,155</span>
+            <span class="apy">APY: 37.70%</span>
+          </div>
+          <div class="info">
+            <div>
+              <p>Total staked</p>
+              <p>
+                <span>1,742,8517</span>
+                <span>SUN</span>
+              </p>
+              <p>
+                <span>561,446,660</span>
+                <span>TRX</span>
+              </p>
+            </div>
+            <div>
+              <p>Pool supply</p>
+              <p>
+                <span>23,127</span>
+                <span>SUN</span>
+              </p>
+            </div>
+          </div>
+          <div class="bottomFix">
+            <a href="#" class="type-link">Get LP Token</a>
+            <button class="create_btn">Select</button>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() { 
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  *{
+    margin :0;
+    padding: 0;
+  }
+  *{
+    box-sizing: border-box;
+  }
+  .sun-page{
+    width: 100%;
+    background-color: #f7f5f5;
+  }
+  .container{
+    width: 76%;
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    padding-top: 60px;
+  }
+  .sun{
+    padding: 20px;
+    width: calc((100% - 60px)/3);
+    background-color: #fff;
+    border-radius: 15px;
+    margin: 10px 20px 10px 0px;
+  }
+  .sun-header{
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    height: 60px;
+  }
+  img.tokenLogo{
+    width: 14%;
+  }
+  img.sunLogo{
+    width: 14%;
+    margin-left: -10px;
+  }
+  .sun-header .header-right{
+    text-align: left;
+    margin-left: 10px;
+  }
+  .sun-header .header-right .title{
+    font-size: 18px;
+  }
+  .sun-header .header-right .hr-sub{
+    font-size: 12px;
+    color: #999;
+    margin-top: 5px;
+  }
+  .sun-header .header-right .hr-sub b{
+    color: #6726eb;
+  }
+  .sun .total{
+    display: flex;
+    justify-content: space-between;
+    margin: 30px 0px;
+    width: 100%;
+  }
+  .sun .total .t-num{
+    font-size: 12px;
+    color: rgba(0,0,0,.65);
+  }
+  .sun .total .apy{
+    font-size: 13px;
+    color: #1ba27a;
+  }
+  .sun .info{
+    width: 100%;
+    padding: 14px 20px;
+    box-shadow: inset 0 -2px 8px 0 rgba(51,1,138,.08);
+    border-radius: 15px;
+  }
+  .sun .info div p:first-child{
+    font-size: 10px;
+    color: #666;
+    line-height: 3;
+  }
+  .sun .info div p:nth-child(n+2){
+    display: flex;
+    justify-content: space-between;
+    font-size: 14px;
+    color: #1a1a1a;
+  }
+  .sun .info div p:nth-child(n+2) span:first-child{
+    display: block;
+    mask-border: -20px;
+  }
+  .sun .info div p:nth-child(n+2) span{
+    font-size: 12px;
+    color: #999;
+    line-height: 2;
+  }
+  .sun .bottomFix{
+    margin-top: 30px;
+    width: 100%;
+  }
+  .sun .bottomFix .type-link{
+    color: #6726eb;
+    font-size: 12px;
+    display: block;
+    text-decoration: none;
+    text-align: center;
+  }
+  .sun .bottomFix .create_btn{
+    width: 100%;
+    display: block;
+    color: #b79cef;
+    font-size: 12px;
+    border-radius: 10px;
+    text-align: center;
+    background: #e2d9f6;
+    border: 0;
+    height: 40px;
+    margin: 13px 0;
+    cursor: not-allowed;
+    outline: none;
+  }
+  @media screen and (max-width: 1200px){
+    .sun-header .header-right .title{
+      font-size: 15px;
+    }
+    .sun-header .header-right .hr-sub{
+      font-size: 9px;
+    }
+    .sun .total .t-num{
+      font-size: 10px;
+    }
+    .sun .total .apy{
+      font-size: 11px;
+    }
+  }
+  @media screen and (max-width: 1000px){
+    .container{
+      width: 90%; 
+    }
+    .container .sun{
+      width: 100%;
+    }
+    .sun .sun-header .tokenLogo{
+      width: 50px;
+    }
+    .sun .sun-header .sunLogo{
+      width: 50px;
+    }
+    .sun-header .header-right .title{
+      font-size: 20px;
+    }
+    .sun-header .header-right .hr-sub{
+      font-size: 15px;
+    }
+    .sun .total .t-num{
+      font-size: 13px;
+    }
+    .sun .total .apy{
+      font-size: 14px;
+    }
+  }
+</style>
