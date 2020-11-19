b9f02bf4f813608f88f9ceeb42e88aca2c7dd200
IT03 <phuc@gmail.com>
2020-11-19 15:59:18 +0700

    sua ti

--- src/components/header.vue
+++ src/components/header.vue
@@ -5,9 +5,9 @@
         <img src="../assets/SUNLogo.178d4636.png" alt="">
       </div>
       <div class="header-menu">
-        <a href="#" class="menu-item" :class="{active:page.sun_page}" @click="show_sun">SUN</a>
-        <a href="#" class="menu-item" :class="{active:page.vote_page}" @click="show_vote">Vote</a>
-        <a href="#" class="menu-item" :class="{active:page.faqs_page}" @click="show_faqs">FAQs</a>
+        <a href="#/sun" class="menu-item" :class="{active:page.sun_page}" @click="show_sun">SUN</a>
+        <a href="#/vote" class="menu-item" :class="{active:page.vote_page}" @click="show_vote">Vote</a>
+        <a href="#/faq" class="menu-item" :class="{active:page.faqs_page}" @click="show_faqs">FAQs</a>
         <a href="#" class="menu-item">Learn</a>
       </div>
       <div class="header-right">
